0bb6184bd11de7e710e321b80f7e10387542b52b
f-rahman23 <f.rahman@iideainformatics.com>
2023-08-17 00:21:24 +0600

    Edited Researches and SidePanel

--- src/Pages/Home/Researches.jsx
+++ src/Pages/Home/Researches.jsx
@@ -1,34 +1,34 @@
 export default function Researches() {
-  return (
-    <div>
-      <h1 className="title">Researches</h1>
-      <ul className="list-disc space-y-10">
-        <li>
-          <h2 className="font-semibold text-lg">APR 2020 - DEC 2020</h2>
-          Worked as a Research Assistant with Dr. Mohammad Abdul Halim,
-          Assistant Professor of Chemistry and Biochemistry at Kennesaw State
-          University, on in-silico promising peptide therapeutics design against
-          SARS-CoV-2 main protease at Red-Green Research Center, Dhaka,
-          Bangladesh.
-        </li>
-        <li>
-          <h2 className=" font-semibold text-lg">DEC 2020 – APR 2021</h2>
-          Worked with Dr. Raihan Sarkar, Assistant Professor, Dept. of
-          Pharmaceutical Technology, University of Dhaka, and K.M. Yasif Kayes
-          Sikder, Assistant Professor, Dept. of Pharmaceutical Technology,
-          University of Dhaka, on solubility enhancement of BCS Class II drugs
-          by solvent evaporation technique at Pharmaceutics Lab, University of
-          Dhaka, Bangladesh.
-        </li>
-        <li>
-          <h2 className=" font-semibold text-lg">DEC 2022 – PRESENT</h2>
-          Currently under the supervision of Dr. Uttom Kumar, Assistant
-          Professor Dept. of Pharmaceutical Technology working on
-          UV-Spectroscopy based method development and formulation development
-          for anti-hypertensive and anti-diabetic drug at Organic Synthesis Lab,
-          Dept. of Pharmaceutical Technology, University of Dhaka
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="title">Researches</h1>
+			<ul className="list-disc space-y-10">
+				<li>
+					<h2 className=" font-semibold text-lg">DEC 2022 – AUG 2023</h2>
+					As a research associate, under the supervision of Dr. Uttom Kumar, Assistant
+					Professor, Dept. of Pharmaceutical Technology, worked on
+					UV-Spectroscopy based method development and formulation development
+					for anti-hypertensive and anti-diabetic drug at Organic Synthesis Lab,
+					Dept. of Pharmaceutical Technology, University of Dhaka
+				</li>
+				<li>
+					<h2 className=" font-semibold text-lg">DEC 2020 – APR 2021</h2>
+					Worked with Dr. Raihan Sarkar, Assistant Professor, Dept. of
+					Pharmaceutical Technology, University of Dhaka, and K.M. Yasif Kayes
+					Sikder, Assistant Professor, Dept. of Pharmaceutical Technology,
+					University of Dhaka, on solubility enhancement of BCS Class II drugs
+					by solvent evaporation technique at Pharmaceutics Lab, University of
+					Dhaka, Bangladesh.
+				</li>
+				<li>
+					<h2 className="font-semibold text-lg">APR 2020 - DEC 2020</h2>
+					Worked as a Research Assistant with Dr. Mohammad Abdul Halim,
+					Assistant Professor of Chemistry and Biochemistry at Kennesaw State
+					University, on in-silico promising peptide therapeutics design against
+					SARS-CoV-2 main protease at Red-Green Research Center, Dhaka,
+					Bangladesh.
+				</li>
+			</ul>
+		</div>
+	);
 }
--- src/Pages/Shared/SidePanel.jsx
+++ src/Pages/Shared/SidePanel.jsx
@@ -1,34 +1,33 @@
 export default function SidePanel() {
-  return (
-    <aside className="p-5 h-100 rounded-lg border order-first lg:order-1">
-      <picture className="flex justify-center">
-        <img
-          className="rounded-lg w-40"
-          src="/Shahoriar.png"
-          alt="Shahoriar's Profile Picture"
-        />
-      </picture>
-      <div className="text-center">
-        <h1 className="text-lg mt-5">Md. Shahoriar Nazir</h1>
-        <p>Bachelor of Pharmacy (B. Pharm)</p>
-        <p>CGPA 3.43 (Out of 4.00) </p>
-        <p>University of Dhaka</p>
-        <h2 className="mt-3">-- Contact Info --</h2>
-        <p>
-          Email:{" "}
-          <a href="mailto:mdshahoriarnazir@gmail.com">
-            mdshahoriarnazir@gmail.com
-          </a>{" "}
-        </p>
-        <p>
-          Phone: <a href="tel:+8801841653737">+8801841653737</a>
-        </p>
-        <h2 className="mt-3">-- Mailing Address --</h2>
-        <p>Dr. Muhammad Shahidullah Hall,</p>
-        <p>Shaheed Ataur Rahman Khan Khadim Bhaban (Ex-2)</p>
-        <p>Room no: 2515, Dr. Muhammad Shahidullah Hall</p>
-        <p>University of Dhaka, Dhaka-1000</p>
-      </div>
-    </aside>
-  );
+	return (
+		<aside className="p-5 h-100 rounded-lg border order-first lg:order-1">
+			<picture className="flex justify-center">
+				<img
+					className="rounded-lg w-40"
+					src="/Shahoriar.png"
+					alt="Shahoriar's Profile Picture"
+				/>
+			</picture>
+			<div className="text-center">
+				<h1 className="text-lg mt-5">Md. Shahoriar Nazir</h1>
+				<p className="font-bold">M. Pharm, B. Pharm</p>
+				<p>University of Dhaka</p>
+				<h2 className="mt-3">-- Contact Info --</h2>
+				<p>
+					Email:{" "}
+					<a href="mailto:mdshahoriarnazir@gmail.com">
+						mdshahoriarnazir@gmail.com
+					</a>{" "}
+				</p>
+				<p>
+					Phone: <a href="tel:+8801841653737">+8801841653737</a>
+				</p>
+				<h2 className="mt-3">-- Mailing Address --</h2>
+				<p>Dr. Muhammad Shahidullah Hall,</p>
+				<p>Shaheed Ataur Rahman Khan Khadim Bhaban (Ex-2)</p>
+				<p>Room no: 2515, Dr. Muhammad Shahidullah Hall</p>
+				<p>University of Dhaka, Dhaka-1000</p>
+			</div>
+		</aside>
+	);
 }
