028ae4e2f7ae44daa4e703d2202f909bf70ad7c7
foy4748 <faisaljfcl@gmail.com>
2023-08-26 12:43:30 +0600

    Fixed some bold issues

--- src/Pages/Home/Publications.jsx
+++ src/Pages/Home/Publications.jsx
@@ -1,15 +1,44 @@
 export default function Publications() {
-	return (
-		<div>
-			<h1 className="title">Publications</h1>
-			<ul className="list-disc space-y-3">
-				<li>
-					Sikdar KY, <strong>Nazir MS</strong>, Alam MM, Sarkar MR, Rahman SA. Evaluation of in <strong>vitro dissolution profile and physicochemical characterization of polymer based formulations of sparingly soluble rosuvastatin</strong>. Dhaka University Journal of Pharmaceutical Sciences. 2021 Dec 29;20(2):199-211.   <a className="link" target="_blank" href="https://www.dujps.com/index.php/recent-issue?download=442:evaluation-of-in-vitro-dissolution-profile-and-physicochemical-characterization-of-polymer-based-formulations-of-sparingly-soluble-rosuvastatin">(DOI: 10.3329/dujps.v20i2.57170)</a>
-				</li>
-				<li>
-					Khan AT, Chowdhury GM, Hafsah J, Maruf M, Raihan MR, Chowdhury MT, Nawal N, Tasnim N, Saha P, Roy P, Tabassum R, Rodrigues SP, Hasan W, Samanta ZT, Kamal S, <strong>Nazir MS</strong>, Ali MA, Halim MA. <strong>A student led computational screening of peptide inhibitors against main protease of SARS‐CoV‐2</strong>. Biochemistry and Molecular Biology Education. 2022 Jan;50(1):7-20.  <a className="link" target="_blank" href="https://iubmb.onlinelibrary.wiley.com/doi/10.1002/bmb.21580">(DOI: 10.1002/bmb.21580)</a>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="title">Publications</h1>
+      <ul className="list-disc space-y-3">
+        <li>
+          Sikdar KY, <strong>Nazir MS</strong>, Alam MM, Sarkar MR, Rahman SA.
+          <strong>
+            {" "}
+            Evaluation of in vitro dissolution profile and physicochemical
+            characterization of polymer based formulations of sparingly soluble
+            rosuvastatin
+          </strong>
+          . Dhaka University Journal of Pharmaceutical Sciences. 2021 Dec
+          29;20(2):199-211.{" "}
+          <a
+            className="link"
+            target="_blank"
+            href="https://www.dujps.com/index.php/recent-issue?download=442:evaluation-of-in-vitro-dissolution-profile-and-physicochemical-characterization-of-polymer-based-formulations-of-sparingly-soluble-rosuvastatin"
+          >
+            (DOI: 10.3329/dujps.v20i2.57170)
+          </a>
+        </li>
+        <li>
+          Khan AT, Chowdhury GM, Hafsah J, Maruf M, Raihan MR, Chowdhury MT,
+          Nawal N, Tasnim N, Saha P, Roy P, Tabassum R, Rodrigues SP, Hasan W,
+          Samanta ZT, Kamal S, <strong>Nazir MS</strong>, Ali MA, Halim MA.{" "}
+          <strong>
+            A student led computational screening of peptide inhibitors against
+            main protease of SARS‐CoV‐2
+          </strong>
+          . Biochemistry and Molecular Biology Education. 2022 Jan;50(1):7-20.{" "}
+          <a
+            className="link"
+            target="_blank"
+            href="https://iubmb.onlinelibrary.wiley.com/doi/10.1002/bmb.21580"
+          >
+            (DOI: 10.1002/bmb.21580)
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/Pages/Home/Researches.jsx
+++ src/Pages/Home/Researches.jsx
@@ -1,33 +1,77 @@
 import SubResearches from "../Shared/SubResearches";
 
 export default function Researches() {
-	return (
-		<div>
-			<SubResearches />
-			<h1 className="title">Research Experiences</h1>
-			<ul className="list-disc space-y-10">
-				<li>
-					<h2 className=" font-semibold text-lg">DEC 2022 – AUG 2023</h2>
-					As a research associate, under the supervision of <a className="text-cyan-500" href="https://www.du.ac.bd/body/faculty_details/PTCH/1484" target={'_blank'}>Dr. Uttom Kumar</a>, Dept. of Pharmaceutical Technology, worked on <strong> UV-Vis based method development and formulation development
-						for anti-hypertensive and anti-diabetic drug</strong> at Organic Synthesis Lab, Dept. of Pharmaceutical Technology, University of Dhaka
-				</li>
-				<li>
-					<h2 className=" font-semibold text-lg">DEC 2020 – APR 2021</h2>
-					Worked with <a className="text-cyan-500" href="https://www.du.ac.bd/faculty/faculty_details/PTCH/1490" target={'_blank'}> Dr. Raihan Sarkar </a>, Dept. of
-					Pharmaceutical Technology, University of Dhaka, and <a className="text-cyan-500" href="https://www.du.ac.bd/faculty/faculty_details/PTCH/2411" target={'_blank'}>K.M. Yasif Kayes
-						Sikder</a> , Dept. of Pharmaceutical Technology,
-					University of Dhaka, on <strong> solubility enhancement of BCS Class II drugs
-						by solid dispersion ( solvent evaporation ) </strong> technique at Pharmaceutics Lab, Dept. of Pharmaceutical Technology, University of Dhaka, Bangladesh.
-				</li>
-				<li>
-					<h2 className="font-semibold text-lg">APR 2020 - DEC 2020</h2>
-					Worked as a Research Assistant with <a className="text-cyan-500" target={'_blank'} href="https://facultyweb.kennesaw.edu/mhalim1/index.php">Dr. Mohammad Abdul Halim</a> ,
-					Associate Professor of Chemistry and Biochemistry at Kennesaw State
-					University, <strong> on in-silico promising peptide therapeutics design against
-						SARS-CoV-2 main protease </strong> at Red-Green Research Center, Dhaka,
-					Bangladesh.
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <SubResearches />
+      <h1 className="title">Research Experiences</h1>
+      <ul className="list-disc space-y-10">
+        <li>
+          <h2 className=" font-semibold text-lg">DEC 2022 – AUG 2023</h2>
+          As a research associate, under the supervision of{" "}
+          <a
+            className="text-cyan-500"
+            href="https://www.du.ac.bd/body/faculty_details/PTCH/1484"
+            target={"_blank"}
+          >
+            Dr. Uttom Kumar
+          </a>
+          , Dept. of Pharmaceutical Technology, worked on{" "}
+          <strong>
+            {" "}
+            UV-Vis based method development and formulation development for
+            anti-hypertensive and anti-diabetic drug
+          </strong>{" "}
+          at Organic Synthesis Lab, Dept. of Pharmaceutical Technology,
+          University of Dhaka
+        </li>
+        <li>
+          <h2 className=" font-semibold text-lg">DEC 2020 – APR 2021</h2>
+          Worked with{" "}
+          <a
+            className="text-cyan-500"
+            href="https://www.du.ac.bd/faculty/faculty_details/PTCH/1490"
+            target={"_blank"}
+          >
+            {" "}
+            Dr. Raihan Sarkar{" "}
+          </a>
+          , Dept. of Pharmaceutical Technology, University of Dhaka, and{" "}
+          <a
+            className="text-cyan-500"
+            href="https://www.du.ac.bd/faculty/faculty_details/PTCH/2411"
+            target={"_blank"}
+          >
+            K.M. Yasif Kayes Sikder
+          </a>{" "}
+          , Dept. of Pharmaceutical Technology, University of Dhaka, on{" "}
+          <strong>
+            {" "}
+            solubility enhancement of BCS Class II drugs by solid dispersion (
+            solvent evaporation ) technique
+          </strong>{" "}
+          at Pharmaceutics Lab, Dept. of Pharmaceutical Technology, University
+          of Dhaka, Bangladesh.
+        </li>
+        <li>
+          <h2 className="font-semibold text-lg">APR 2020 - DEC 2020</h2>
+          Worked as a Research Assistant with{" "}
+          <a
+            className="text-cyan-500"
+            target={"_blank"}
+            href="https://facultyweb.kennesaw.edu/mhalim1/index.php"
+          >
+            Dr. Mohammad Abdul Halim
+          </a>{" "}
+          , at Kennesaw State University, on
+          <strong>
+            {" "}
+            in-silico promising peptide therapeutics design against SARS-CoV-2
+            main protease{" "}
+          </strong>{" "}
+          at Red-Green Research Center, Dhaka, Bangladesh.
+        </li>
+      </ul>
+    </div>
+  );
 }
